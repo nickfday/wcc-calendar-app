59d3b849e9c7eda2c86ff2b0fb24f8fa4048fed0
Nick Finley-Day <nickfday@gmail.com>
2017-03-07 11:54:31 +0000

    exerciseItem class

--- src/Components/Exercise/ExerciseList.js
+++ src/Components/Exercise/ExerciseList.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
 import { Table } from 'reactstrap';
 
+class ExerciseItem extends Component {
+	render() {
+		return (
+			<div>
+				 <tr>
+            <td><img src="http://fitnessremoted7.dev/sites/default/files/stock-photo-48646366-bodybuilder-performing-dips.jpg" alt="" /></td>
+            <td>Bench Press</td>
+            <td>Chest</td>
+            <td>Triceps</td>
+            <td>Beginner</td>
+          </tr>
+          <tr>
+            <td><img src="http://fitnessremoted7.dev/sites/default/files/stock-photo-48646366-bodybuilder-performing-dips.jpg" alt="" /></td>
+            <td>Bench Press</td>
+            <td>Chest</td>
+            <td>Triceps</td>
+            <td>Beginner</td>
+          </tr>
+			</div>
+			)
+	}
+}
+
 
 class ExerciseList extends Component {
 
@@ -20,20 +43,7 @@ class ExerciseList extends Component {
         </thead>
         <tbody>
 
-          <tr>
-            <td><img src="http://fitnessremoted7.dev/sites/default/files/stock-photo-48646366-bodybuilder-performing-dips.jpg" alt="" /></td>
-            <td>Bench Press</td>
-            <td>Chest</td>
-            <td>Triceps</td>
-            <td>Beginner</td>
-          </tr>
-          <tr>
-            <td><img src="http://fitnessremoted7.dev/sites/default/files/stock-photo-48646366-bodybuilder-performing-dips.jpg" alt="" /></td>
-            <td>Bench Press</td>
-            <td>Chest</td>
-            <td>Triceps</td>
-            <td>Beginner</td>
-          </tr>
+       <ExerciseItem />
 
         </tbody>
       </Table>
