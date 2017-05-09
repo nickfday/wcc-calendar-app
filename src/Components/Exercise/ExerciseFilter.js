31b85b39762a3ecb3654cb05adefe0eb0641291e
Nick Finley-Day <nickfday@gmail.com>
2017-05-09 12:13:35 +0100

    added data bind for search exercise

---
+++ src/Components/Exercise/ExerciseFilter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class ExerciseFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.handleFilterTextInputChange = this.handleFilterTextInputChange.bind(this);
+}
+
+	handleFilterTextInputChange(e) {
+	  this.props.onFilterTextInput(e.target.value);
+	}
+
+  render() {
+
+  	return(
+  		<div>
+  			<h3>Filters</h3>
+  			<form>
+  				<input type="text"
+  				placeholder="Search..."
+  				value={this.props.filterText}
+  				onChange={this.handleFilterTextInputChange}
+  				 />
+  			</form>
+  		</div>
+  	)
+  }
+}
+
+export default ExerciseFilter;
--- src/Components/Exercise/ExerciseList.js
+++ src/Components/Exercise/ExerciseList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Table } from 'reactstrap';
 import ExerciseRow from './ExerciseRow';
+import ExerciseFilter from './ExerciseFilter';
 var Loader = require('react-loader');
 import axios from 'axios';
 
@@ -12,18 +13,23 @@ class ExerciseList extends Component {
 		super();
 		this.state = {
 			exercises:[],
-			loaded: false
+			loaded: false,
+			filterText: ''
 		}
+		this.handleFilterTextInput = this.handleFilterTextInput.bind(this);
+	}
+
+	handleFilterTextInput(filterText) {
+		this.setState({
+			filterText: filterText
+		})
 	}
 
 	getExercises(){
     const self = this;
     axios.get('http://fitnessremoted7.dev/api/rest/views/exercise.json')
     .then(function(response) {
-    	//console.log(`axios is ${this}`);
-    	//console.log(data);
     	 self.setState({exercises: response.data, loaded: true}, function(){
-          //console.log(this.state);
         });
     })
     .catch(function(error) {
@@ -42,10 +48,23 @@ class ExerciseList extends Component {
 
   render() {
 
+  	var rows = [];
+  	var exerciseList = this.state;
+  	this.state.exercises.forEach(function(row){
+  		if ((exerciseList.filterText !='') && (row.title.indexOf(exerciseList.filterText) === -1)) {
+  			return;
+  		}
+  	 rows.push( <ExerciseRow exercises={row} key={row.uuid} />);
+  	});
+
   	return(
   		<div className="content exercise-list container">
   		<Loader loaded={this.state.loaded}>
     		<h1>Exercises</h1>
+    		<ExerciseFilter
+    		  filterText={this.state.filterText}
+    		  onFilterTextInput={this.handleFilterTextInput}
+    		/>
 	     	<Table responsive>
 	     	<thead>
 	           <tr>
@@ -57,9 +76,10 @@ class ExerciseList extends Component {
 	           </tr>
 	         </thead>
 	         <tbody>
-	     			{this.state.exercises.map(exercise =>
+	     			{/*{this.state.exercises.map(exercise =>
          			  <ExerciseRow exercises={exercise} key={exercise.uuid} />
-       			)}
+       			)}*/}
+       			{rows}
 	     	  	</tbody>
 	     	</Table>
 	     	</Loader>
