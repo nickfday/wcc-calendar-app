1ff9f545a0fccf34c176fb462bf425b163bc9302
Nick Finley-Day <nickfday@gmail.com>
2017-07-25 12:20:48 +0000

    changed to let

--- src/Components/Misc/Helper.js
+++ src/Components/Misc/Helper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export function splitMap(string, separator, className) {
-  var list = string.split(separator).map((i, key) => { //warning  'list' is already defined  no-redeclare
+  let list = string.split(separator).map((i, key) => { //warning  'list' is already defined  no-redeclare
     return (
       <div className={className} key={key}>{i}</div>
     )
